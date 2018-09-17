657f5a6a8d194c5aadac668f6bf4f7862201b567
Alexander Olofsson <Phenomphenom@live.se>
2018-09-17 15:19:38 +0200

    Add project files.

---
+++ rednit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rednit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
