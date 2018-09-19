2a6d15a3d54e79f5912483efd4245e776d069cf0
ZokiSama <joakim.hall1@gmail.com>
2018-09-19 13:13:34 +0200

    Add Varibles and Construc in User

--- rednit/Domain/User.cs
+++ rednit/Domain/User.cs
@@ -6,5 +6,16 @@ namespace rednit.Domain
 {
     class User
     {
+        public string UserID { get; }
+        public string FirstName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        
+        public User(string firstName, string gender, int age)
+        {
+            FirstName = firstName;
+            Gender = gender;
+            Age = age;
+        }
     }
 }
