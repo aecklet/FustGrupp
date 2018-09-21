e6cd6754da96a664edd391ff818758910e36fd2b
Joakim Persson <Joakiimm@hotmail.com>
2018-09-21 10:05:53 +0200

    Added Meny

--- rednit/Program.cs
+++ rednit/Program.cs
@@ -6,7 +6,41 @@ namespace rednit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool meny = true;
+            while (meny)
+            {
+                Console.WriteLine("[1] Create Account");
+                Console.WriteLine("[2] Login");
+                Console.WriteLine("[3] Quit");
+
+                int menySelect = Convert.ToInt32(Console.ReadLine());
+
+                switch (menySelect)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Create account");
+
+                            break;
+                        }
+
+                    case 2:
+                        {
+
+                            Console.WriteLine("Login");
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            meny = false;
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
