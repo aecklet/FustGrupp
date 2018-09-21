b66e7cfdf1416b80dbda1b15e57784320e7c1ec9
ZokiSama <joakim.hall1@gmail.com>
2018-09-21 13:42:57 +0200

    added preferd Gender

--- rednit/Domain/User.cs
+++ rednit/Domain/User.cs
@@ -6,16 +6,18 @@ namespace rednit.Domain
 {
     class User
     {
-        public string UserID { get; }
+        // public int UserID { get; set; }
         public string FirstName { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
-        
-        public User(string firstName, string gender, int age)
+        public string PrefGender { get; set; }
+
+        public User(string firstName, string gender, int age, string prefGender)
         {
             FirstName = firstName;
             Gender = gender;
             Age = age;
+            PrefGender = prefGender;
         }
     }
 }
---
+++ rednit/Domain/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace rednit.Domain
+{
+    class Utils
+    {
+        public HashCode GetID(string strInput)
+        {
+            Random rnd = new Random(strInput.GetHashCode());
+            return rnd;
+        }
+    }
+}
